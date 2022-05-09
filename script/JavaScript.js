76b86a825b5a6d1a170f25d906151bf685754fbc
ndeguchi <natsudeguchi@gmail.com>
2022-05-09 10:45:07 -0700

    initial comment

---
+++ script/JavaScript.js
@@ -0,0 +1,64 @@
+
+
+/* nav dropdown function */
+
+function dropdown(){
+  const menu = document.getElementById('menu');
+  const hidden = 'hidden';
+  const display = 'display';
+
+  if(menu.classList.contains('hidden')){
+      menu.classList.add(display);
+      menu.classList.remove(hidden);
+  }else{
+    menu.classList.add(hidden);
+    menu.classList.remove(display);
+  }
+}
+
+   
+/* changing button (play <-> pause) and changing title color (white <-> green) */
+
+function changeBtn() {
+     const play = document.getElementById('play');
+     const pause = document.getElementById('pause');
+     const playingText = document.getElementById('playingText');
+     const playingTextP = document.getElementById('playingTextP');
+     const nowPlaying = document.getElementById('nowPlaying');
+     const playNumber = document.getElementById('playNumber');
+     const hidden = 'hidden';
+     const display = 'display';
+     const color = 'color';
+     const color2 = 'color2';
+   
+     if (play.classList.contains('hidden')) {
+       
+       play.classList.add(display);
+       play.classList.remove(hidden);
+
+       playingText.classList.add(color);
+       playingTextP.classList.add(color2);
+       nowPlaying.classList.add(display);
+
+       playNumber.classList.add(hidden);
+       playNumber.classList.remove(display);
+       
+       // hiding
+       pause.classList.add(hidden);
+       pause.classList.remove(display);
+     } else {
+       
+       pause.classList.add(display);
+       pause.classList.remove(hidden);
+
+       playNumber.classList.add(display);
+       playNumber.classList.remove(hidden);
+      
+       // hiding
+       play.classList.add(hidden);
+       play.classList.remove(display);
+       
+       playingText.classList.remove(color);
+       nowPlaying.classList.remove(display);
+     }
+   }
